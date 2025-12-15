1a441c93d6c29595e6fdbe7bde8cbac969399e42
Aldiosebastiaan <aldiosebastian9@gmail.com>
2025-12-16 00:18:41 +0700

    mengembalikan file main.jsx ke project awal

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
-import Exercise from "./Exercise.jsx";
-import PostCard from "./PostCard.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/posts" replace />} />
-        <Route path="/posts" element={<PostCard />} />
-        <Route path="/exercise" element={<Exercise />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
