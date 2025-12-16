c75aaec77346435f4b4dda1a05828dc347d49a8b
Aldiosebastiaan <aldiosebastian9@gmail.com>
2025-12-16 09:30:42 +0700

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { div } from "framer-motion/client";
 import "./App.css";
 import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/error";
 
 function App() {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Halaman Utama</div>,
+      errorElement: <ErrorPage/>,
+    },
+    {
+      path: "/login",
+      element: <SignInPage/>,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage/>
+    },
+  ]);
+
   return (
     <>
-      <SignInPage />
+      <RouterProvider router={myRouter} />
     </>
   );
 }
